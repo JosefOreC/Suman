f939fc45982fc210a2e98328dd6d0b5e4ee18569
Josef <71660137@continental.edu.pe>
2025-03-21 13:10:38 -0500

    Se creó una función suma

--- operaciones.py
+++ operaciones.py
@@ -1,4 +1,4 @@
 sumando1 = float(input("Primer sumando: "))
 sumando2 = float(input("Segundo sumando: "))
 suma = sumando1+sumando2
-print(f"{sumando1}+{sumando2}={suma}")
+print(f"{sumando1}+{sumando2}={suma}\n¿Es correcto?")
